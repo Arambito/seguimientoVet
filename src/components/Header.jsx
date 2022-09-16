38d060116d87984055ef33c2c6a4a7ab6887bfbc
Bito <alexandersantisarellano@gmail.com>
2022-09-16 13:31:41 -0300

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <>
         <h1 className="font-black text-5xl text-center mr-10 w-2/3 mx-auto">
-            Seguimiento pacientes {''}
+            Seguimiento pacientes {''} con GitHub {''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
         
